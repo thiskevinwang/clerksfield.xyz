9fc001c5351471bd0011973270af933179adfdd0
Kevin Wang <kwangsan@gmail.com>
2026-07-24 11:36:03 -0400

    update proxy handler

--- app/%5Fclerk/[[...path]]/route.ts
+++ app/%5Fclerk/[[...path]]/route.ts
@@ -1,6 +1,9 @@
-import { createFrontendApiProxyHandlers } from "@clerk/nextjs/server";
 import { clerkProxyPath } from "@/clerk-proxy";
+import { createFrontendApiProxyHandlers } from "@clerk/nextjs/server";
 
 export const { GET, POST, PUT, DELETE, PATCH } = createFrontendApiProxyHandlers({
   proxyPath: clerkProxyPath,
+  fapiUrl: process.env.CLERK_FAPI_URL,
+  publishableKey: process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
+  secretKey: process.env.CLERK_SECRET_KEY,
 });
